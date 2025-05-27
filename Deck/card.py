1c4b8fe3d5af7a0d1071f6a9193449b6b944c2f7
kfscully <scullykirby@gmail.com>
2025-05-27 13:19:13 -0700

    cleaning up name property

--- Deck/card.py
+++ Deck/card.py
@@ -1,30 +1,27 @@
 suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
 rank_values = list(range(2,15))
-value_names = {2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten"}
+value_names = {2: "Two", 3: "Three", 4: "Four", 5: "Five",
+               6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten"}
 face_values = {11: "Jack", 12: "Queen", 13: "King", 14: "Ace"}
 
 class Card:
     """
     This defines a card
     """
-    def __init__(self, value, suit, name):
+    def __init__(self, value, suit):
         self.value = value
-        self.suit = suit
-        self._name = name
+        self.suit = suit.capitalize()
 
     @property
     def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if value in value_names:
-            name = f"{value_names[self.value]} of {suits[self.suit]}"
-        if value in face_values:
-            name = f"{face_values[self.value]} of {suits[self.suit]}"
+        if self.value in value_names:
+            str_name = value_names[self.value]
+        if self.value in face_values:
+            str_name = face_values[self.value]
+        return f"{str_name} of {self.suit}"
 
     def __repr__(self):
-        return(f"Card({self.suit}, {self.value})")
+        return(f"Card({self.value}, {self.suit})")
 
     def __str__(self):
-        return(f"This card is the {self._name}")
+        return(f"This card is the {self.name}")
