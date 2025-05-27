df71c2d7fe41e96915cc711eee66fade4121aabf
kfscully <scullykirby@gmail.com>
2025-05-27 13:23:22 -0700

    fixing flake8 errors

--- Deck/card.py
+++ Deck/card.py
@@ -1,8 +1,9 @@
 suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
-rank_values = list(range(2,15))
+rank_values = list(range(2, 15))
 value_names = {2: "Two", 3: "Three", 4: "Four", 5: "Five",
                6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten"}
 face_values = {11: "Jack", 12: "Queen", 13: "King", 14: "Ace"}
+
 
 class Card:
     """
@@ -21,7 +22,7 @@ class Card:
         return f"{str_name} of {self.suit}"
 
     def __repr__(self):
-        return(f"Card({self.value}, {self.suit})")
+        return f"Card({self.value}, {self.suit})"
 
     def __str__(self):
-        return(f"This card is the {self.name}")
+        return f"This card is the {self.name}"
